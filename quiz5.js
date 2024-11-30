d39fbe84bdc9d0610d7f3f60901ee24cbe4f5346
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-11-30 11:00:55 +0330

    5

---
+++ quiz5.js
@@ -0,0 +1,12 @@
+//? 5. Write a code to show if there are any truthy values in the array below
+//? hint:use just one of this methods and pass the isTruthy function to that method
+//? some(), every(), includes(), slice()
+const array = ['0', null, '', 0];
+function isTruthy(num) {
+  return Boolean(num)
+}
+
+// !  Answer:
+const result = array.some(isTruthy);
+console.log(result);
+
