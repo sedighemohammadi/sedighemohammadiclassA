ab1f2796bb880788a2e35603dba299f4d3e608ac
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-11-30 11:00:24 +0330

    1

---
+++ quiz1.js
@@ -0,0 +1,8 @@
+//? 1. What is the difference between setTimeout and setInterval ?
+//!  Answer:
+
+
+
+
+// Use setTimeout for one-time delays.
+// - Use setInterval for repeated actions at fixed intervals.
