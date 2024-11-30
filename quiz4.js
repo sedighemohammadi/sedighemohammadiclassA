ebad6b687cd69eca5c431aae249047bc44f9a050
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-11-30 11:00:48 +0330

    4

---
+++ quiz4.js
@@ -0,0 +1,6 @@
+//? 4. Write an array with five elements for this function in order to the result become true
+
+const array = [14 , 15 , 45 , 11 ,155]; // to be completed
+
+// DO NOT modify below code
+console.log(array.every((num) => num > 10));
